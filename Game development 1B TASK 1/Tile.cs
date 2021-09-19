857549aa46cafe1962211c9aac51debd9d8f4c5a
LeagoMokoena <leagonok@gmail.com>
2021-09-19 11:12:35 +0200

    Update Tile.cs class with two private and public accessory variables, a public enum with the type of tiles, a contracture that receives values and sets them to the fields

--- Game development 1B TASK 1/Tile.cs
+++ Game development 1B TASK 1/Tile.cs
@@ -4,7 +4,38 @@ using System.Text;
 
 namespace Game_development_1B_TASK_1
 {
-    class Tile
+    abstract class Tile
     {
+        private int x;
+
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        private int y;
+
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public enum Tiletype
+        {
+            Hero,
+            Enemy,
+            Gold,
+            Weapon
+        }
+
+        public Tiletype tiletype;
+        public Tile(int _xPosition, int _yPosition, Tiletype symbol)
+        {
+            x = _xPosition;
+            y = _yPosition;
+            tiletype = symbol;
+        }
     }
 }
