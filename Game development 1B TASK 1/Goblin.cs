5ca437825669d987367e3c81902dd08a1eb4f54b
LeagoMokoena <leagonok@gmail.com>
2021-09-19 13:04:39 +0200

    Created a Goblin.cs class that checks the direction a boblin moves and sets features of the goblins

---
+++ Game development 1B TASK 1/Goblin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game_development_1B_TASK_1
+{
+    class Goblin:Enemy
+    {
+        public Goblin(int Xposition,int Yposition)
+        {
+            Xposition = X;
+            Yposition = Y;
+            HP = 10;
+            Damage = 1;
+        }
+
+        public override int Returnmove()
+        {
+            Random r = new Random();
+            Xposition = r.Next();
+        }
+    }
+}
