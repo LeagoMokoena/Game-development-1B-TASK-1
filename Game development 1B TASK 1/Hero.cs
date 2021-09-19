7ce39b9a7426d83869152f93e507939a89c2fb1d
LeagoMokoena <leagonok@gmail.com>
2021-09-19 13:08:26 +0200

    Created a Hero.cs class that sets the features for the hero like HP

---
+++ Game development 1B TASK 1/Hero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game_development_1B_TASK_1
+{
+    class Hero:Character
+    {
+        public Hero(int _xposition, int _yposition, int _hp) : this(_xposition, _yposition)
+        {
+            _xposition = X;
+            _yposition = Y;
+            _hp = HP;
+            Damage = 2;
+        }
+    }
+}
