d2faf1abf377a193b64821300de45790c88a1fa0
LeagoMokoena <leagonok@gmail.com>
2021-09-19 12:57:41 +0200

    Created Enemy.cs class that receives and enemies HP, max HP, damage, x and y position and a symbol for the enemy

---
+++ Game development 1B TASK 1/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game_development_1B_TASK_1
+{
+    abstract class Enemy : Character
+    {
+
+        private Random objectNum = new Random();
+
+        public Enemy(int xPosition,int yPosition,int enemyDamage,int startHP,int maxHP, Tiletype symbol):this(xPosition,yPosition)
+        {
+            
+            enemyDamage = Damage; 
+            xPosition = X;
+            yPosition = Y;
+            maxHP = MaxHp;
+            startHP = HP;
+            symbol = tiletype;
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine("at" + "[" + X + "," + Y + "]");
+        }
+    }
+}
