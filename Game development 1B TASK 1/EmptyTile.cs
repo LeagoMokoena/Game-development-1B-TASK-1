485e33bd8897c0d1c20c02a0a75cc97ce3a9a294
LeagoMokoena <leagonok@gmail.com>
2021-09-19 11:36:08 +0200

    Create EmptyTile.cs

---
+++ Game development 1B TASK 1/EmptyTile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game_development_1B_TASK_1
+{
+    class EmptyTile:Tile
+    {
+        public EmptyTile(int xparameter,int yparameter,Tiletype symbol) : base(xparameter, yparameter, symbol)
+        {
+        }
+    }
+}
