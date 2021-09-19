183d73e5db12029ec8572c8bd9d24564b29578ae
LeagoMokoena <leagonok@gmail.com>
2021-09-19 17:43:52 +0200

    Created Map.cs class with variables holding the hero and the enemy, variables holding the width and height of the map, arrays to hold the enemies and hero's ,and method to place objects on the map

---
+++ Game development 1B TASK 1/Map.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game_development_1B_TASK_1
+{
+    class Map
+    {
+        private Tile [,]mapCharacter;
+
+        public Tile [,] MapCharacter
+        {
+            get { return mapCharacter; }
+            set { mapCharacter = value; }
+        }
+
+        private Hero theHero;
+
+        public Hero TheHero
+        {
+            get { return theHero; }
+            set { theHero = value; }
+        }
+
+        private Enemy [,] theEnemies;
+
+        public Enemy [,] TheEnemies
+        {
+            get { return theEnemies; }
+            set { theEnemies = value; }
+        }
+
+        private int width;
+
+        public int Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+
+        private int height;
+
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+        private Random newItem;
+
+        public Random NewItem
+        {
+            get { return newItem; }
+            set { newItem = value; }
+        }
+
+
+        public Map(int minWidth,int maxWidth,int minHeight,int maxHeight,int enemyNum)
+        {
+            width = newItem.Next(minWidth, maxWidth);
+            height = newItem.Next(minHeight, maxHeight);
+            mapCharacter = new Tile[width, height];
+            theEnemies = new Enemy[width, height]; 
+        }
+
+        public void UpdateVision()
+        {
+            foreach(Tile s in theHero.Vision)
+                
+        }
+
+        private Tile create()
+        {
+
+        }
+
+
+    }
+}
