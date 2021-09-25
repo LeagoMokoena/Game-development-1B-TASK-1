5627518a4a1503f6ae3834b9139f58b2dc696c97
LeagoMokoena <leagonok@gmail.com>
2021-09-25 15:28:56 +0200

    Updated the gameEngine.cs class to return string for the the view of the map. Also made a method that returns the movement of the player if it is allowed

--- Game development 1B TASK 1/GameEngine.cs
+++ Game development 1B TASK 1/GameEngine.cs
@@ -23,36 +23,47 @@ namespace Game_development_1B_TASK_1
         public bool MovePlayer(Character.movement direction)
         {
             bool move = false;
+            direction = area.TheHero.Returnmove(direction);
+
             if (direction != Character.movement.noMovement)
             {
-                switch (direction)
-                {
-                    case Character.movement.Up:
-                        area.mapCharacter[area.TheHero.X - 1, area.TheHero.Y] = new Obstacle(area.TheHero.X - 1, area.TheHero.Y, Tile.Tiletype.Gold, area.Width, area.Height);
-                        break;
-                    case Character.movement.Down:
-                        area.mapCharacter[area.TheHero.X + 1, area.TheHero.Y] = new Obstacle(area.TheHero.X + 1, area.TheHero.Y, Tile.Tiletype.Gold, area.Width, area.Height);
-                        break;
-                    case Character.movement.Right:
-                        area.mapCharacter[area.TheHero.X, area.TheHero.Y + 1] = new Obstacle(area.TheHero.X, area.TheHero.Y + 1, Tile.Tiletype.Gold, area.Width, area.Height);
-                        break;
-                    case Character.movement.Left:
-                        area.mapCharacter[area.TheHero.X, area.TheHero.Y - 1] = new Obstacle(area.TheHero.X, area.TheHero.Y - 1, Tile.Tiletype.Gold, area.Width, area.Height);
-                        break;
-                }
-                move = true;
+                area.mapCharacter[area.TheHero.X, area.TheHero.Y] = area.Empty_Tile;
+                area.TheHero.Move(direction);
             }
 
-            return move;
+            area.UpdateVision();
+                return move;
         }
 
         public override string ToString()
         {
-            return base.ToString(area.MapCharacter[,]);
+            string mapDesign = "";
+
+            area.TheHero.Symbol = Hero;
+            area.ObstacleX.Symbol = Obstacle;
+            area.Empty_Tile.Symbol = Emptytile;
+            for(int x = 0; x < area.TheEnemies.Length; x++)
+            {
+                area.TheEnemies[x].Symbol = enemy;
+            }
+
+            for (int i = 0; i < area.Width; i++)//for loop displaying the values in the map array in the game map
+            {
+                for (int j = 0; j < area.Height; j++)
+                {
+
+                    mapDesign += area.mapCharacter[i, j].Symbol;
+                }
+
+                mapDesign += "\n";
+            }
+
+            return mapDesign;
         }
 
         private static readonly char enemy = 'G';
         private static readonly char Hero = 'H';
         private static readonly char Obstacle = 'X';
+        private static readonly char Emptytile = '.';
     }
 }
