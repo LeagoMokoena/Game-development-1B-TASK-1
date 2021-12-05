8f38876e0ea1b13ff6f2eabed0f0b71911057861
LeagoMokoena <leagonok@gmail.com>
2021-12-05 11:13:40 +0200

    Updated shop class. Class now creates weapon class objects that are stored in the weapon array randomly.

--- Game development 1B TASK 1/Shop.cs
+++ Game development 1B TASK 1/Shop.cs
@@ -28,10 +28,58 @@ namespace Game_development_1B_TASK_1
 
         private Weapon RandomWeapon()
         {
-            List<MeleeWeapon.Types> values = Enum.GetValues(typeof(MeleeWeapon.Types)).Cast<MeleeWeapon.Types>().ToList();
-            List<RangedWeapons.Types> values2 = Enum.GetValues(typeof(RangedWeapons.Types)).Cast<RangedWeapons.Types>().ToList();
-            Random weapon = new Random();
-            weapon.Next(values, values2);
+            char weap;
+            weap = (char)ran.Next('a', 'd');
+            if (weap == 'a')
+            {
+                MeleeWeapon newMeleeWeapon = new MeleeWeapon(1, 1, Tile.Tiletype.Weapon, 'D', MeleeWeapon.Types.Dagger);
+                return newMeleeWeapon;
+            }
+            else if(weap == 'b')
+            {
+                MeleeWeapon newMeleeWeapon = new MeleeWeapon(1, 1, Tile.Tiletype.Weapon, 'S', MeleeWeapon.Types.Longsword);
+                return newMeleeWeapon;
+            }
+            else if(weap == 'c')
+            {
+                RangedWeapons newRangedweapon = new RangedWeapons(1, 1, Tile.Tiletype.Weapon, 'R', RangedWeapons.Types.Rifle);
+                return newRangedweapon;
+            }
+            else
+            {
+                RangedWeapons newRangedweapon = new RangedWeapons(1, 1, Tile.Tiletype.Weapon, 'B', RangedWeapons.Types.Longbow);
+                return newRangedweapon;
+            }
+        }
+
+        public bool CanBuy(int num)
+        {
+            if(buyer.GoldPurse.Count == defences[num].Cost)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Buy(int num)
+        {
+            int count = buyer.GoldPurse.Count;
+            for(int i = 0; i < defences[num].Cost; i++)
+            {
+                buyer.GoldPurse.RemoveAt(count);
+                count--;
+            }
+
+            buyer.Pickup(defences[num]);
+            defences[num] = RandomWeapon();
+        }
+
+        public string DisplayWeapon(int num)
+        {
+            return "Buy " + defences[num].WeaponType + " (" + defences[num].Cost + " Gold)";
         }
     }
 }
